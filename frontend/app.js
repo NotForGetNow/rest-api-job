b87daeb5897fc508cd40376a0e7f721bb0f0d4b4
Ануар Канат Ануарулы <171016428@step.kz>
2024-06-28 19:40:20 +0500

    A

--- frontend/app.js
+++ frontend/app.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:4000/api/items'
+const API_URL = 'https://rest-api-jobbbb.onrender.com'
 
 document.getElementById('dataForm').addEventListener('submit', async function (event) {
     event.preventDefault()
